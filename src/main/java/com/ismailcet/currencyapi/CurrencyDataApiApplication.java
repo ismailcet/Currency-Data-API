cc5492adcf0a21a14210cde673d14c9fc2759365
ismailcet <ismailcet3@gmail.com>
2023-07-02 23:17:59 +0300

    Currency API Task 3 Starter Files

---
+++ src/main/java/com/ismailcet/currencyapi/CurrencyDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.ismailcet.currencyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyDataApiApplication.class, args);
+	}
+
+}
